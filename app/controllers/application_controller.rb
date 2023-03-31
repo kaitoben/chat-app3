d3508b69ce2c71c25c958751c542dcdaa96b9b16
kaitoben <3312ben@gmail.com>
2023-03-31 11:26:42 +0900

    ユーザー編集機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!#deviseのメソッドでログインしていないユーザーはログイン画面に戻される
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  
+private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
+
+
